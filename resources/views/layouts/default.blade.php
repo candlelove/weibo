f26aae9bfbff4f6a7ab72cfcdfdd3ff46202aeea
PonyMa <77607022@qq.com>
2020-08-27 08:21:27 +0000

    静态文件浏览器缓存问题

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title>@yield('title','Weibo App')-Laravel学习</title>
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="{{mix('css/app.css')}}">
 </head>
 
 <body>
