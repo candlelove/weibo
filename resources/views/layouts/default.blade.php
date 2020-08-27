f3f3aa810964c4fb88261620d8e61c9db4e1e6f9
PonyMa <77607022@qq.com>
2020-08-27 04:53:20 +0000

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,10 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <title>@yield('title','Weibo App')-Laravel学习</title>
+    <meta charset="UTF-8">
+    <title>@yield('title','Weibo App')-Laravel学习</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
+
 <body>
-  @yield('content')
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="/">Weibo App</a>
+            <ul class="navbar-nav justify-content-end">
+                <li class="nav-item"><a href="/help" class="nav-link">帮助</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">登录</a></li>
+            </ul>
+        </div>
+
+    </nav>
+
+    @yield('content')
 </body>
+
 </html>
