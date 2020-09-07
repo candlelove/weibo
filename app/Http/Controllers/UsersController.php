6a51e40659baadff643ba0a6fd0504724ff191a8
PonyMa <77607022@qq.com>
2020-09-07 08:03:22 +0000

    访问策略

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,6 +10,17 @@ use Auth;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth',[
+            'except' => ['show','create','store']
+        ]);
+
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
     public function create()
     {
         return view('users.create');
@@ -43,11 +54,13 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
-        return view('users.edit',compact('user'));
+        $this->authorize('update', $user);
+        return view('users.edit', compact('user'));
     }
 
     public function update(User $user, Request $request)
     {
+        $this->authorize('update',$user);
         $this->validate($request,[
             'name' => 'required|max:50',
             'password' => 'required|confirmed|min:6'
@@ -62,7 +75,7 @@ class UsersController extends Controller
         $user->update($data);
         session()->flash('success','个人资料更新成功！');
 
-        return redirect()->route('users.show',$user);
+        return redirect()->route('users.show',$user->id);
     }
 
 
