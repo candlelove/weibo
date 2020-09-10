8cd19d17c39000f67f7321405d49c44a51fa91d9
PonyMa <77607022@qq.com>
2020-09-10 13:23:06 +0000

    临时

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         // $this->call(UserSeeder::class);
         Model::unguard();
         $this->call(UserTableSeeder::class);
+        $this->call(StatusesTableSeeder::class);
         Model::reguard();
     }
 }
