08af26627dc2741d47b03796a52664dc6f82806e
PonyMa <77607022@qq.com>
2020-09-21 12:21:02 +0000

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -62,8 +62,12 @@ class User extends Authenticatable
 
     public function feed()
     {
-        return $this->statuses()
-                    ->orderBy('created_at','desc');
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids,$this->id);
+
+        return Status::WhereIn('user_id',$user_ids)
+                                ->with('user')
+                                ->orderBy('created_at','desc');
     }
 
     public function followers()
@@ -96,4 +100,6 @@ class User extends Authenticatable
     {
         return $this->followings->contains($user_id);
     }
+
+
 }
